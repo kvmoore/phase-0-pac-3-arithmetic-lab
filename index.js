8e9c43da3488486866e5f4473c355dbdb7b945b2
Kenneth <kvmoore097@gmail.com>
2023-06-10 10:05:04 -0500

    Arthmetic lab

---
+++ index.js
@@ -0,0 +1,64 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a,b) {
+
+    return a + b ;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    
+    return a * b;
+}
+
+function divide(a, b) {
+
+    return a / b;
+}
+
+
+
+function increment(n) {
+
+return n+=1;
+
+
+}function decrement(n) {
+
+    return n-+1;
+}
+
+
+
+function makeInt(n) {
+
+    return parseInt(n , 10);
+
+
+}function preserveDecimal(n) {
+
+    return parseFloat(n);
+
+}
